ee063195c2465b818824ba8cbf31b1d0e7b521d8
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-01-30 16:07:38 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#3-misol
+items = [1, 2, 2, 3, 3, 4, 1]
+
+items.remove(1)
+items.remove(2)
+items.remove(3)
+items.sort()
+print(items)
+
+items.sort()
+print(items)
+
+
+items.sort()
+print(items)
+
+print(f"Birinvhi element {items[0]}")
